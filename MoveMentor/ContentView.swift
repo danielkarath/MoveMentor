1684708d26a574b04f777e317fac121925802ac6
danielkarath <karath.dani@gmail.com>
2023-12-06 14:09:01 +0100

    Initial Commit

---
+++ MoveMentor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoveMentor
+//
+//  Created by Daniel Karath on 12/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoveMentor/MoveMentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveMentorApp.swift
+//  MoveMentor
+//
+//  Created by Daniel Karath on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MoveMentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
